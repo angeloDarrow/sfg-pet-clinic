5352e21e3080e170f78b52bfd73d1c3b72ed7ee0
angeloDarrow <aggelis.davelos@gmail.com>
2020-03-20 19:50:15 +0200

    updata base entity JPA Annotation. closes #35

--- pet-clinic-data/src/main/java/com/angelo/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/angelo/sfgpetclinic/model/BaseEntity.java
@@ -1,11 +1,17 @@
 package com.angelo.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass//base class, all other classes gonna inherit from this
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
 
     public Long getId() {
         return id;
--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,11 +1,11 @@
-package com.angelo.sfgpetclinic;
+package sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest(classes = com.angelo.sfgpetclinic.SfgPetClinicApplication.class)
 @ContextConfiguration(classes = com.angelo.sfgpetclinic.SfgPetClinicApplication.class)
+@SpringBootTest(classes = com.angelo.sfgpetclinic.SfgPetClinicApplication.class)
 class SfgPetClinicApplicationTests {
 
     @Test
