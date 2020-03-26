7308fadd6f22abccc89893214b4f6c8a94615600
DAVELOS Angelis <adavelos@intrasoft-intl.com>
2020-03-26 14:13:26 +0200

    updated profile for jpa services. closes #48

--- pet-clinic-data/src/main/java/com/angelo/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/angelo/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,11 +2,13 @@ package com.angelo.sfgpetclinic.services.map;
 
 import com.angelo.sfgpetclinic.model.Pet;
 import com.angelo.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
--- pet-clinic-data/src/main/java/com/angelo/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/angelo/sfgpetclinic/services/map/VetServiceMap.java
@@ -4,11 +4,13 @@ import com.angelo.sfgpetclinic.model.Specialty;
 import com.angelo.sfgpetclinic.model.Vet;
 import com.angelo.sfgpetclinic.services.SpecialtySevice;
 import com.angelo.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialtySevice specialtySevice;
